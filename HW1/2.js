bb13d880f7a6080027c6a8d1a91de8689b0e89d5
vandoyn <dn130575gss@gmail.com>
2022-01-12 15:51:38 +0200

    ok

---
+++ HW1/2.js
@@ -0,0 +1,27 @@
+/*
+Перепишите `if..else` с использованием нескольких операторов `?`.
+
+Для читаемости — оформляйте код в несколько строк.
+
+```js
+var message;
+
+if (login == 'Pitter') {
+message = 'Hi';
+} else if (login == 'Owner') {
+message = 'Hello';
+} else if (login == '') {
+message = 'unknown';
+} else {
+message = '';
+}
+```
+*/
+
+var login = '';
+var message;
+message = (login == 'Pitter') ? 'Hi' :
+ (login == 'Owner') ? 'Hello' : 
+ (login == '') ? 'unknown' : 
+ '';
+console.log(message)
---
+++ HW1/3.js
@@ -0,0 +1,30 @@
+/*
+Перепишите код, заменив оператор `switch` на оператор `if..else`:
+
+```js
+switch (val) {
+case 'a':
+console.log( 'a' );
+break;
+
+case 'b':
+case 'c':
+case 'd':
+case 'e':
+console.log( 'others' );
+break;
+
+default:
+console.log( 'unknown' );
+}
+```
+*/
+
+var val = 'd';
+if (val == 'a') {
+    console.log( 'a' );
+} else if (val == 'b' || val == 'c' || val == 'd' || val == 'e') {
+    console.log( 'others' );
+} else {
+    console.log( 'unknown' )
+}
