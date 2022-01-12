9779e54725b7c4c07f427c66894eea403de35018
vandoyn <dn130575gss@gmail.com>
2022-01-12 15:45:56 +0200

    ok

---
+++ HW1/1.js
@@ -0,0 +1,16 @@
+/*
+Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+```js
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}
+```
+
+*/
+var result;
+var a=1, b=3;
+result=(a + b < 4) ? true : false;
--- Task1/1.js
+++
@@ -1,5 +0,0 @@
-var x=1;
-while (x>5) {
-    console.log(x);
-    x++;
-}
